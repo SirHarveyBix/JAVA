b3802a3cf2d0ba3459e4bd9286ece799c9164203
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-09-06 11:51:27 +0200

    If + code blocks

--- _01-basics/src/Hello.java
+++ _01-basics/src/Hello.java
@@ -1,6 +1,12 @@
 public class Hello {
+
     public static void main(String[] args){
         System.out.println("Hello, Tim");
-        System.out.print("Hello World!");
+
+        boolean isAlien = false;
+        if(isAlien == false) { // Equality Operator
+            System.out.println("It is not an Alien!");
+            System.out.println("And I am scared of Aliens!");
+        }
     }
 }
