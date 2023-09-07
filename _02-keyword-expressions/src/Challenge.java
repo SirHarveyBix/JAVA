baa2c45ab1c3bcdecead90a1adab6444adf282e6
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-09-07 12:49:02 +0200

    playing around with methods

--- _02-keyword-expressions/src/Challenge.java
+++ _02-keyword-expressions/src/Challenge.java
@@ -7,30 +7,43 @@ public class Challenge {
         int levelCompleted = 5;
         int bonus = 100;
 
-        int finalScore = score;
-
-        calculateScore(gameOver, score, levelCompleted, bonus);
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your highScore = " + highScore);
 
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
 
-        finalScore = score;
+        System.out.println("The next highScore = " +
+                calculateScore(gameOver, score, levelCompleted, bonus));
 
-        if (gameOver) {
-            finalScore += (levelCompleted * bonus);
-            System.out.println("Your final score = " + finalScore);
-        }
+        calculateHighScorePosition(1000);
+        calculateHighScorePosition(750);
+        calculateHighScorePosition(100);
+        calculateHighScorePosition(50);
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         int finalScore = score;
 
         if (gameOver) {
             finalScore+= (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score = " + finalScore);
+        }
+        return finalScore;
+    }
+
+    public static void calculateHighScorePosition(int score) {
+
+        if (score >= 1000) {
+            System.out.println("score >= 1000 = 1");
+        } else if (score >= 500 && score < 1000) {
+            System.out.println("score >= 500 && score < 1000 = 2");
+        } else if (score >= 100 && score < 500) {
+            System.out.println("score >= 100 && score < 500 = 3");
+        } else {
+            System.out.println("else = 4");
         }
     }
 }
