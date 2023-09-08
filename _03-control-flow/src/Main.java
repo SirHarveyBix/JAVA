2271e650384593ea1ebd4c307657cfeeb9ec6e7f
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-09-08 17:19:38 +0200

    enhenced swithch statement

--- _03-control-flow/src/Main.java
+++ _03-control-flow/src/Main.java
@@ -1,17 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int value = 2;
-
+        int value = 3;
+// switch case value types : byte, short, int, char, String, enum.
         switch (value) {
-            case 1:
-                System.out.println("Value was 1");
-                break;
-            case 2:
-                System.out.println("Value was 2");
-                break;
-            default:
-                System.out.println("Value was not 1 or 2");
-                break;
+            case 1 -> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> {
+                System.out.println("Value 3, 4, or 5");
+                System.out.println("Actually it was : " + value);
+            }
+            default -> System.out.println("Value was not 1, 2, 3, 4, or 5");
         }
     }
 
