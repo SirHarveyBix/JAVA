b54b32a650f6e30200008643b4efc37ad0c54461
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-09-08 17:09:26 +0200

    switch cases

--- _03-control-flow/src/Main.java
+++ _03-control-flow/src/Main.java
@@ -1,13 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int value = 1;
+        int value = 2;
 
-        if (value == 1) {
-            System.out.println("Value was 1");
-        } else if (value == 2) {
-            System.out.println("Value was 2");
-        } else {
-            System.out.println("Value was not 1 or 2");
+        switch (value) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            default:
+                System.out.println("Value was not 1 or 2");
+                break;
         }
     }
 
