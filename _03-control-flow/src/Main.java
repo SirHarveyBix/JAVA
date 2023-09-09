7994a7134241c4a0b29fe73c9abecc2d4a5aafdd
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-09-09 12:24:24 +0200

    create tradication switch

--- _03-control-flow/src/Main.java
+++ _03-control-flow/src/Main.java
@@ -13,6 +13,8 @@ public class Main {
         }
         Months month = Months.April;
         System.out.println(month + " is in the " + getQuarter(month) + " quarter");
+
+        getNatoLetter('Z');
     }
 
     public static String getQuarter(Months months) {
@@ -31,4 +33,86 @@ public class Main {
         };
     }
 
+    public static void getNatoLetter(char letter) {
+        switch (letter) {
+            case 'A':
+                System.out.println("Able");
+                break;
+            case 'B':
+                System.out.println("Baker");
+                break;
+            case 'C':
+                System.out.println("Charlie");
+                break;
+            case 'D':
+                System.out.println("Dog");
+                break;
+            case 'E':
+                System.out.println("Easy");
+                break;
+            case 'F':
+                System.out.println("Fox");
+                break;
+            case 'G':
+                System.out.println("George");
+                break;
+            case 'H':
+                System.out.println("How");
+                break;
+            case 'I':
+                System.out.println("Item");
+                break;
+            case 'J':
+                System.out.println("Jig");
+                break;
+            case 'K':
+                System.out.println("King");
+                break;
+            case 'L':
+                System.out.println("Love");
+                break;
+            case 'M':
+                System.out.println("Mike");
+                break;
+            case 'N':
+                System.out.println("Nan");
+                break;
+            case 'P':
+                System.out.println("Oboe");
+                break;
+            case 'Q':
+                System.out.println("Peter");
+                break;
+            case 'R':
+                System.out.println("Queen");
+                break;
+            case 'S':
+                System.out.println("Roger");
+                break;
+            case 'T':
+                System.out.println("Sugar");
+                break;
+            case 'U':
+                System.out.println("Uncle");
+                break;
+            case 'V':
+                System.out.println("Victor");
+                break;
+            case 'W':
+                System.out.println("William");
+                break;
+            case 'X':
+                System.out.println("X-ray");
+                break;
+            case 'Y':
+                System.out.println("Yoke");
+                break;
+            case 'Z':
+                System.out.println("Zebra");
+                break;
+            default:
+                System.out.println(letter + " :  Not found");
+                break;
+        }
+    }
 }
