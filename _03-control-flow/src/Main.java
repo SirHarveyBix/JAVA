e82db7a9bcdd94a1497c153101ba721b800390ce
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-09-09 12:41:24 +0200

    coding exercices

--- _03-control-flow/src/Main.java
+++ _03-control-flow/src/Main.java
@@ -17,6 +17,72 @@ public class Main {
         getNatoLetter('Z');
     }
 
+    public static boolean isLeapYear(int year) {
+        if ((year < 1) || (year >= 9999)) {
+            return false;
+        } else return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+    }
+
+    public static int getDaysInMonth(int month, int year) {
+        if (((month < 1) || (month > 12)) || ((year < 1) || (year > 9999))) {
+            return -1;
+        }
+        if (isLeapYear(year) && month == 2) {
+            return 29;
+        }
+
+        switch (month) {
+            case 11:
+            case 4:
+            case 6:
+            case 9:
+                return 30;
+            case 2:
+                return 28;
+            default:
+                return 31;
+        }
+
+    }
+
+    public static void printNumberInWord(int number) {
+        switch (number) {
+            case 0:
+                System.out.println("ZERO");
+                break;
+            case 1:
+                System.out.println("ONE");
+                break;
+            case 2:
+                System.out.println("TWO");
+                break;
+            case 3:
+                System.out.println("THREE");
+                break;
+            case 4:
+                System.out.println("FOUR");
+                break;
+            case 5:
+                System.out.println("FIVE");
+                break;
+            case 6:
+                System.out.println("SIX");
+                break;
+            case 7:
+                System.out.println("SEVEN");
+                break;
+            case 8:
+                System.out.println("EIGHT");
+                break;
+            case 9:
+                System.out.println("NINE");
+                break;
+            default:
+                System.out.println("OTHER");
+                break;
+        }
+    }
+
     public static String getQuarter(Months months) {
 
         return switch (months) {
