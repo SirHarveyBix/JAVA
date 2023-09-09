9807fc8573e26ea3c657a2da4fb618303a54313f
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-09-09 16:34:44 +0200

    implement while loop + do & continue; / break;

---
+++ _03-control-flow/src/WhileAndDo.java
@@ -0,0 +1,45 @@
+public class WhileAndDo {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 5; i++) {
+            System.out.println(i);
+        }
+        System.out.println(" # ---- ---- ---- # ");
+
+        int j = 1;
+        while (j <= 5) {
+//        while (true) {
+//            if (j > 5) {
+//                break;
+//            }
+            System.out.println(j);
+            j++;
+        }
+        j = 1;
+        System.out.println(" # ---- ---- ---- # ");
+
+        boolean isReady = false;
+
+        do {
+            if (j > 5) {
+                break;
+            }
+            System.out.println(j);
+            j++;
+            isReady = (j > 0);
+
+        } while (isReady);
+
+        System.out.println(" # ---- Continue Statement ---- # ");
+
+        int number = 0;
+        while (number < 50) {
+            number += 5;
+
+            if (number % 25 == 0) {
+                continue;
+            }
+            System.out.print(number + ", ");
+        }
+    }
+}
