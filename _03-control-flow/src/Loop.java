784f07203399599201e822050dddea5948a10d31
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-09-09 13:54:46 +0200

    add exercisies : ways to exiting loops

--- _03-control-flow/src/Loop.java
+++ _03-control-flow/src/Loop.java
@@ -1,27 +1,39 @@
 public class Loop {
     public static void main(String[] args) {
-        for (int counter = 1; counter <= 5; counter++) {
-            System.out.println(counter);
-        }
-
-        System.out.println(" # ---- ---- ---- # ");
 
         double amount = 100.00;
 
-        for (double rate = 2.0; rate <= 5.0; rate++) {
+        for (double rate = 7.5; rate <= 10; rate += 0.25) {
             double interestAmount = calculateInterest(amount, rate);
+
+            if (interestAmount > 8.5) break;
             System.out.println(buildString(amount, rate, interestAmount));
         }
-
         System.out.println(" # ---- ---- ---- # ");
 
-        for (double rate = 7.5; rate <= 10; rate += 0.25) {
+        int count = 0;
 
-            double interestAmount = calculateInterest(amount, rate);
-            if (interestAmount > 8.5) break;
-
-            System.out.println(buildString(amount, rate, interestAmount));
+        for (int number = 0; number <= 5; number++) {
+            if (count == 5) break; // not necessary here because of : number <= 5
+            if (isPrime(number)) {
+                count++;
+            }
+            System.out.println(number + " is " + (isPrime(number) ? "" : "NOT ") + "a prime number.");
         }
+        System.out.println("Total number of prime number is " + count);
+    }
+
+    public static boolean isPrime(int wholeNumber) {
+        if (wholeNumber <= 2) {
+            return (wholeNumber == 2);
+        }
+
+        for (int divisor = 2; divisor < wholeNumber; divisor++) {
+            if (wholeNumber % divisor == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static String buildString(double amount, double rate, double interest) {
