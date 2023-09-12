f9295bf81006fdbf5f3a9f96783c08def40aa203
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-09-12 11:20:28 +0200

    add default values, getter, setter

--- _04-OOP-1_inheritence/src/Car.java
+++ _04-OOP-1_inheritence/src/Car.java
@@ -1,9 +1,50 @@
 public class Car {
     // fields :
-    private String make;
-    private String model;
-    private String color;
-    private int doors;
-    private boolean convertible;
+    private String make = "Tesla"; // default value
+    private String model = "Model X";
+    private String color = "Gray";
+    private int doors = 2;
+    private boolean convertible = true;
 
+    // Setters
+    public void setMake(String make) {
+        this.make = make;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
+
+    // Getters
+    public String getModel() {
+        return model;
+    }
+    public String getColor() {
+        return color;
+    }
+    public int getDoors() {
+        return doors;
+    }
+    public boolean isConvertible() {
+        return convertible;
+    }
+    public String getMake(){
+        return make;
+    }
+
+    public void describeCar() {
+        System.out.println(doors + "-Doors " +
+                color + " " +
+                make + " " +
+                model + " " +
+                (convertible ? "Convertible" : ""));
+    }
 }
--- _04-OOP-1_inheritence/src/Main.java
+++ _04-OOP-1_inheritence/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
+        Car car = new Car();
+//        car.make = "Porsche";
+//        car.model = "Carrera";
+//        car.color = "Red";
+        System.out.println("Make = " + car.getMake());
+        car.describeCar();
     }
 }
