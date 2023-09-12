ba9a94fd60116c9f0b2484000bad89bc43142f35
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-09-12 13:15:11 +0200

    add validator + use getters and setters

--- _04-OOP-1_inheritence/src/Car.java
+++ _04-OOP-1_inheritence/src/Car.java
@@ -6,23 +6,6 @@ public class Car {
     private int doors = 2;
     private boolean convertible = true;
 
-    // Setters
-    public void setMake(String make) {
-        this.make = make;
-    }
-    public void setModel(String model) {
-        this.model = model;
-    }
-    public void setColor(String color) {
-        this.color = color;
-    }
-    public void setDoors(int doors) {
-        this.doors = doors;
-    }
-    public void setConvertible(boolean convertible) {
-        this.convertible = convertible;
-    }
-
     // Getters
     public String getModel() {
         return model;
@@ -38,6 +21,31 @@ public class Car {
     }
     public String getMake(){
         return make;
+    }
+
+    // Setters
+    public void setMake(String make) {
+        // Validation
+        if (make == null) make = "Unknown";
+        String lowercaseMake = make.toLowerCase();
+        switch (lowercaseMake){
+            case "holden", "porsche", "tesla"-> this.make = make;
+            default -> {
+                this.make ="Unsupported";
+            }
+        }
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
     }
 
     public void describeCar() {
--- _04-OOP-1_inheritence/src/Main.java
+++ _04-OOP-1_inheritence/src/Main.java
@@ -2,10 +2,25 @@ public class Main {
     public static void main(String[] args) {
 
         Car car = new Car();
-//        car.make = "Porsche";
-//        car.model = "Carrera";
-//        car.color = "Red";
+//        car.make = "Porsche"; // => can't access this way since make is private; instead uses setter
+        car.setMake("Porsche");
+        car.setModel("Carrera");
+        car.setDoors(2);
+        car.setColor("Black");
+        car.setConvertible(true);
+
         System.out.println("Make = " + car.getMake());
+        System.out.println("Model = " + car.getModel());
+
         car.describeCar();
+
+        Car targa = new Car();
+        targa.setMake("Porsche");
+        targa.setModel("Targa");
+        targa.setDoors(2);
+        car.setConvertible(false);
+        targa.setColor("Red");
+
+        targa.describeCar();
     }
 }
