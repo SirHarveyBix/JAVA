c4c8306d26266c710c68f7dc408d50412dc8dfef
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-09-25 11:52:41 +0200

    fix error for FloorExercise + made it runnable

--- _04-OOP-1_inheritence/src/exercices/FloorExercise.java
+++ _04-OOP-1_inheritence/src/exercices/FloorExercise.java
@@ -1,19 +1,21 @@
 package exercices;
 
 public class FloorExercise {
-
     public static void main(String[] args) {
         Carpet carpet = new Carpet(3.5);
         Floor floor = new Floor(2.75, 4.0);
         Calculator calculator = new Calculator(floor, carpet);
-        System.out.println("total= " + calculator.getTotalCost());
+
+        System.out.println("\ntotal = " + calculator.getTotalCost());
+
         carpet = new Carpet(1.5);
         floor = new Floor(5.4, 4.5);
         calculator = new Calculator(floor, carpet);
-        System.out.println("total = " + calculator.getTotalCost());
+
+        System.out.println("total = " + calculator.getTotalCost() + "\n");
     }
 
-    //    Floor.java
+    // Floor.java
     // public class Floor {
     static class Floor {
         private double width;
