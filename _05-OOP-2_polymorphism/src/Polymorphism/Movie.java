0b79cddc2fecba5c80fd0dbd9a88965d5728a0f9
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-09-29 13:09:21 +0200

    add Scanner and swith to complete moplymorphism example

--- _05-OOP-2_polymorphism/src/Polymorphism/Movie.java
+++ _05-OOP-2_polymorphism/src/Polymorphism/Movie.java
@@ -12,6 +12,15 @@ public class Movie {
 
         System.out.println(title + " is a " + instanceType + " film");
     }
+
+    public static Movie getMovie(String type, String title) {
+        return switch (type.toUpperCase().charAt(0)) {
+            case 'A' -> new Adventure(title);
+            case 'C' -> new Comedy(title);
+            case 'S' -> new ScienceFiction(title);
+            default -> new Movie(title);
+        };
+    }
 }
 
 class Adventure extends Movie {
@@ -46,6 +55,6 @@ class ScienceFiction extends Movie {
     @Override
     public void watchMovie() {
         super.watchMovie();
-        System.out.printf(".. %s%n".repeat(3), "Bad Aliens do Bad Stuff", "Space Guys chase Aliens", "Planet BLows Up");
+        System.out.printf(".. %s%n".repeat(3), "Bad Aliens do Bad Stuff", "Space Guys Chase Aliens", "Planet Blows Up");
     }
 }
