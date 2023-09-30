804c91ba65049c451dc07bcd63b92d14cb0961f8
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-09-30 16:00:24 +0200

    complete burger challenge

--- _05-OOP-2_polymorphism/src/OOP_MasterChallenge/Main.java
+++ _05-OOP-2_polymorphism/src/OOP_MasterChallenge/Main.java
@@ -2,11 +2,25 @@ package OOP_MasterChallenge;
 
 public class Main {
     public static void main(String[] args) {
-        Item coke = new Item("drink", "coke", 1.50);
-        coke.printItem();
-        coke.setSize("LARGE");
-        coke.printItem();
+//        Item coke = new Item("drink", "coke", 1.50);
+//        coke.printItem();
+//        coke.setSize("LARGE");
+//        coke.printItem();
+//
+//        Item avocado = new Item("Topping", "avocado", 1.5);
 
-        Item avocado = new Item("Topping", "avocado", 1.5);
+//        Burger burger = new Burger("regular", 4.0);
+//        burger.addToppings("BACON", "CHEESE", "MAYO");
+//        burger.printItem();
+
+//        MealOrder regularMeal = new MealOrder();
+//        regularMeal.addBurgerTopping("BACON", "CHEESE", "MAYO");
+//        regularMeal.setDrinkSize("LARGE");
+//        regularMeal.printItemizedList();
+
+    MealOrder secondMeal = new MealOrder("turkey", "7-up", "chili");
+    secondMeal.addBurgerTopping("LETTUCE", "CHEESE", "MAYO");
+    secondMeal.setDrinkSize("SMALL");
+    secondMeal.printItemizedList();
     }
 }
