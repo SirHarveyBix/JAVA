a41c85b2e88fb29166567d7d718e2069d7b36070
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-10-02 12:16:52 +0200

    enhenced loop for more fun with arrays + Arrays.toString()

--- _06-arrays/src/arrays/Main.java
+++ _06-arrays/src/arrays/Main.java
@@ -1,4 +1,5 @@
 package arrays;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,14 +19,34 @@ public class Main {
 
         int arrayLength = firstTen.length;
         System.out.println("length of array = " + arrayLength);
-        System.out.println("length of array = " + firstTen[arrayLength - 1]); // as js arrays stats at 0
+        System.out.println("length of array = " + firstTen[arrayLength - 1]); // as js arrays starts at 0
 
         System.out.println("\n # # # # # \n");
 
         int[] newArray;
-        newArray = new int[]{5, 4, 3, 2, 1};
+//      newArray = new int[]{5, 4, 3, 2, 1};
+        newArray = new int[5];
         for (int i = 0; i < newArray.length; i++) {
-            System.out.println(newArray[i] + " ");
+            newArray[i] = newArray.length - i;
         }
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.print(newArray[i] + " ");
+        }
+        System.out.println("\n");
+
+        for (int element : newArray) {
+            System.out.print(element + " ");
+        }
+        System.out.println("\n" + Arrays.toString(newArray)); // > to print out the array variable
+
+        Object objectVariable = newArray;
+        if (objectVariable instanceof int[]) {
+            System.out.println("objectVariable is Really an int array");
+        }
+
+        Object[] objectArray = new Object[3];
+        objectArray[0] = "Hello";
+        objectArray[1] = new StringBuilder("World");
+        objectArray[2] = newArray;
     }
 }
