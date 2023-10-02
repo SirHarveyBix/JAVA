e4eea3c16c0d855b235cf92ec35e3c81bddb2442
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-10-02 13:59:37 +0200

    playing with arrays methods : Arrays.equals, Arrays.binarySearch

--- _06-arrays/src/UsingArrays/Main.java
+++ _06-arrays/src/UsingArrays/Main.java
@@ -42,6 +42,24 @@ public class Main {
         int[] largeArray = Arrays.copyOf(thirdArray, 15);
         System.out.println(Arrays.toString(largeArray));
 
+        System.out.println("\n # # Sort & binarySearch # # \n");
+        String[] stringArray = {"Able", "Jane", "Mark", "Ralph", "David"};
+        Arrays.sort(stringArray);
+        System.out.println(Arrays.toString(stringArray));
+        if (Arrays.binarySearch(stringArray, "Mark") >= 0) { // return index
+            System.out.println("Found Mark in the list!");
+        }
+
+        System.out.println("\n # # Arrays.equals # # \n");
+        int[] s1 = {1, 2, 3, 4, 5};
+        int[] s2 = {1, 2, 3, 4, 5};
+        if (Arrays.equals(s1, s2)) { // check out length + values positions
+            System.out.println("Arrays are equal");
+        } else {
+            System.out.println("Arrays are not equal");
+
+        }
+
     }
 
     public static int[] getRandomArray(int len) {
