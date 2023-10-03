5bac40d3c386a484397730cf6ce06af567eab6ef
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-10-03 16:35:20 +0200

    init List & ArrayList + playing with record

--- _07-List_ArrayList_LinkedList_Iterator_Autoboxing/src/ArrayList/Main.java
+++ _07-List_ArrayList_LinkedList_Iterator_Autoboxing/src/ArrayList/Main.java
@@ -1,7 +1,34 @@
 package ArrayList;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+record GroceryItem(String name, String type, int count) {
+    public GroceryItem(String name) {
+        this(name, "DIARY", 1);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
+        /* Object[] groceryArray = new Object[3]; */
+        GroceryItem[] groceryArray = new GroceryItem[3];
+
+        groceryArray[0] = new GroceryItem("milk");
+        groceryArray[1] = new GroceryItem("apples", "PRODUCE", 6);
+        /*groceryArray[2] = "5 oranges";*/
+        groceryArray[2] = new GroceryItem("oranges", "PRODUCE", 5);
+
+        System.out.println(Arrays.toString(groceryArray));
+
+        ArrayList objectList = new ArrayList();
+        objectList.add(new GroceryItem("Butter"));
+        objectList.add("Yogurt");
+
+        ArrayList<GroceryItem> groceryList = new ArrayList<GroceryItem>();
+        groceryList.add(new GroceryItem("Butter"));
+
+        System.out.println("groceryList: " + groceryList);
 
     }
 }
