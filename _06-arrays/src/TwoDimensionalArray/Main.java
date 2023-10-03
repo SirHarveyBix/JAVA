c6272f8e2a4a319e484d395e6d4118a473389b09
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-10-03 15:53:59 +0200

    add more complex nested arrays

--- _06-arrays/src/TwoDimensionalArray/Main.java
+++ _06-arrays/src/TwoDimensionalArray/Main.java
@@ -19,7 +19,7 @@ public class Main {
             var innerArray = array2[i];
             for (int j = 0; j < innerArray.length; j++) {
 //                System.out.println(array2[i][j] + " ");
-            array2[i][j] = (i * 10) + (j+1);
+                array2[i][j] = (i * 10) + (j + 1);
             }
             System.out.println();
         }
@@ -33,5 +33,38 @@ public class Main {
 //        }
 
         System.out.println(Arrays.deepToString(array2));
+        System.out.println("\n # # # # # \n");
+
+        array2[1] = new int[]{16, 26, 36};
+        System.out.println(Arrays.deepToString(array2));
+        System.out.println("\n # # # # # \n");
+
+        Object[] anyArray = new Object[3];
+        System.out.println(Arrays.toString(anyArray));
+        System.out.println("\n # # # # # \n");
+
+        anyArray[0] = new String[]{"a", "b", "c"};
+        System.out.println(Arrays.deepToString(anyArray));
+        System.out.println("\n # # # # # \n");
+
+        anyArray[1] = new String[][]{
+                {"1", "2"},
+                {"2", "3", "4"},
+                {"6", "7", "8", "9"},
+        };
+        System.out.println(Arrays.deepToString(anyArray));
+        System.out.println("\n # # # # # \n");
+
+        anyArray[2] = new int[2][2][2];
+//        anyArray[2] = "Hello";
+        System.out.println(Arrays.deepToString(anyArray));
+        System.out.println("\n # # # # # \n");
+
+        for (Object element : anyArray) {
+            System.out.println("Element type: " + element.getClass().getSimpleName());
+            System.out.println("Element toString(): " + element);
+            System.out.println(Arrays.deepToString((Object[]) element));
+            System.out.println("# # # # # \n");
+        }
     }
 }
