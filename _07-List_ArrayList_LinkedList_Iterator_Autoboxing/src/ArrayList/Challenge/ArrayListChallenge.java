9ab3a438db841315e9742384e474e95dd96d5e7c
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-10-04 13:10:14 +0200

    add: ArrayList challenge (using Scanner)

---
+++ _07-List_ArrayList_LinkedList_Iterator_Autoboxing/src/ArrayList/Challenge/ArrayListChallenge.java
@@ -0,0 +1,61 @@
+package ArrayList.Challenge;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class ArrayListChallenge {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean flag = true;
+        ArrayList<String> groceries = new ArrayList<>();
+
+        while (flag) {
+            printActions();
+            switch (Integer.parseInt(scanner.nextLine())) {
+                case 1 -> addItems(groceries);
+                case 2-> removeItems(groceries);
+                default -> flag = false;
+            }
+            groceries.sort(Comparator.naturalOrder());
+            System.out.println(groceries);
+        }
+    }
+
+    private static void addItems(ArrayList<String> groceries) {
+        System.out.println("Add item(s) [separate items by comma]:");
+        String[] items = scanner.nextLine().split(",");
+//        groceries.addAll(List.of(items));
+
+        for (String index : items) {
+            String trimmed = index.trim();
+            if (groceries.indexOf(trimmed) < 0) {
+                groceries.add(trimmed);
+            }
+        }
+    }
+
+    private static void removeItems(ArrayList<String> groceries) {
+        System.out.println("Remove item(s) [separate items by comma]:");
+        String[] items = scanner.nextLine().split(",");
+
+        for (String index : items) {
+            String trimmed = index.trim();
+                groceries.remove(trimmed);
+        }
+    }
+
+    private static void printActions() {
+        String textBlock = """
+                Available action:
+                        0 - shutdown
+
+                        1 - add item(s) to list (comma delimited)
+
+                        2 - remove item(s) (comma delimited)
+
+                        Enter a number for witch action you want to do:""";
+        System.out.println(textBlock + " ");
+    }
+}
