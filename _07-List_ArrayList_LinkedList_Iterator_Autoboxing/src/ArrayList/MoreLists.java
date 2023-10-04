8b92a4974e3357663efe232530de5ce237d0b151
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-10-04 11:33:23 +0200

    playing with lists methods :remove, removeAll, retainAll & Comparator

--- _07-List_ArrayList_LinkedList_Iterator_Autoboxing/src/ArrayList/MoreLists.java
+++ _07-List_ArrayList_LinkedList_Iterator_Autoboxing/src/ArrayList/MoreLists.java
@@ -1,6 +1,8 @@
 package ArrayList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class MoreLists {
@@ -22,5 +24,46 @@ public class MoreLists {
 
         groceries.addAll(nextList);
         System.out.println("groceries: " + groceries);
+
+        if (groceries.contains("mustard")) {
+            System.out.println("List contains 'mustard'");
+        }
+
+        System.out.println("\n # # # # #\n");
+
+        groceries.add("yogurt");
+        System.out.println("first = " + groceries.indexOf("yogurt"));
+        System.out.println("last = " + groceries.lastIndexOf("yogurt"));
+        System.out.println("groceries = " + groceries);
+
+        groceries.remove(1);
+        System.out.println("groceries = " + groceries);
+
+        groceries.remove("yogurt");
+        System.out.println("groceries = " + groceries);
+
+        groceries.removeAll(List.of("apples", "eggs"));
+        System.out.println("groceries = " + groceries);
+
+        groceries.retainAll(List.of("apples", "milk", "mustard", "cheese", "nothing"));
+        System.out.println("groceries = " + groceries);
+
+        groceries.clear();
+        System.out.println("groceries = " + groceries);
+        System.out.println("groceries isEmpty = " + groceries.isEmpty());
+
+        groceries.addAll(List.of("apples", "milk", "mustard", "cheese"));
+        groceries.addAll(Arrays.asList("eggs", "pickles", "mustard", "ham"));
+        System.out.println("groceries = " + groceries);
+
+        groceries.sort(Comparator.naturalOrder());
+        System.out.println("groceries = " + groceries);
+
+        groceries.sort(Comparator.reverseOrder());
+        System.out.println("groceries = " + groceries);
+
+        var groceryArray = groceries.toArray(new String[groceries.size()]);
+        System.out.println("groceryArray = " + Arrays.toString(groceryArray));
+
     }
 }
