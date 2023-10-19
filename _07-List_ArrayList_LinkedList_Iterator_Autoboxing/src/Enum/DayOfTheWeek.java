4aa1fb8d19f54c4ca8aeded2222fc524560287aa
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-10-19 20:13:19 +0200

    playin with enums

--- _07-List_ArrayList_LinkedList_Iterator_Autoboxing/src/Enum/DayOfTheWeek.java
+++ _07-List_ArrayList_LinkedList_Iterator_Autoboxing/src/Enum/DayOfTheWeek.java
@@ -4,6 +4,7 @@ public enum DayOfTheWeek {
     SUN,
     MON,
     TUES,
+    WED,
     THURS,
     FRI,
     SAT
--- _07-List_ArrayList_LinkedList_Iterator_Autoboxing/src/Enum/Main.java
+++ _07-List_ArrayList_LinkedList_Iterator_Autoboxing/src/Enum/Main.java
@@ -13,9 +13,21 @@ public class Main {
 
             System.out.printf("Name is %s, Ordinal Value = %d%n", weekDay.name(), weekDay.ordinal());
 
-            if(weekDay == DayOfTheWeek.FRI){
+            if (weekDay == DayOfTheWeek.FRI) {
                 System.out.println("Found a Friday !");
             }
+        }
+
+        System.out.println("\n# # # # #\n");
+
+        for (int i = 0; i < 10; i++) {
+            weekDay = getRandomDay();
+            switchDayOfWeek(weekDay);
+        }
+        System.out.println("\n# # # # #\n");
+
+        for (Topping topping : Topping.values()) {
+            System.out.println(topping.name() + " : " + topping.getPrice());
         }
     }
 
@@ -24,5 +36,19 @@ public class Main {
         var allDays = DayOfTheWeek.values();
 
         return allDays[randomInteger];
+    }
+
+    public static void switchDayOfWeek(DayOfTheWeek weekDay) {
+        int weekDayInteger = weekDay.ordinal() + 1;
+
+        switch (weekDay) {
+            case WED -> System.out.println("Wednesday is day " + weekDayInteger);
+            case SAT -> System.out.println("Saturday is day " + weekDayInteger);
+            default -> System.out.println(
+                    weekDay.name().charAt(0) +
+                            weekDay.name().substring(1).toLowerCase() +
+                            "day is Day " + weekDayInteger
+            );
+        }
     }
 }
