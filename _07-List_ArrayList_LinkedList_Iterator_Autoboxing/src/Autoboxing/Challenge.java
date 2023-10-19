b13d5a9a161ea77b91e38dde8ac434b98a73fea4
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-10-19 19:38:26 +0200

    add some methods

--- _07-List_ArrayList_LinkedList_Iterator_Autoboxing/src/Autoboxing/Challenge.java
+++ _07-List_ArrayList_LinkedList_Iterator_Autoboxing/src/Autoboxing/Challenge.java
@@ -14,6 +14,19 @@ public class Challenge {
 
         Customer bob = new Customer("Bob S.", 1000);
         System.out.println(bob);
+        System.out.println("\n# # # # #\n");
+
+        Bank bank = new Bank("Chase");
+        bank.addNewCustomer("jane A", 500.0);
+        System.out.println(bank);
+
+        bank.addTransaction("Jane A", -10.25);
+        bank.addTransaction("Jane A", -75.01);
+        bank.printStatement("Jane A");
+
+        bank.addNewCustomer("bob s", 25);
+        bank.addTransaction("Bob S", 100);
+        bank.printStatement("Bob S");
     }
 }
 
@@ -23,6 +36,14 @@ class Bank {
 
     public Bank(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Bank{" +
+                "name='" + name + '\'' +
+                ", customers=" + customers +
+                '}';
     }
 
     private Customer getCustomer(String customerName) {
@@ -40,6 +61,29 @@ class Bank {
             Customer customer = new Customer(customerName, initialDeposit);
             customers.add(customer);
             System.out.println("New Customer added: " + customer);
+        }
+    }
+
+    public void addTransaction(String name, double transactionAmount) {
+        Customer customer = getCustomer(name);
+
+        if (customer != null) {
+            customer.transaction().add(transactionAmount);
+        }
+    }
+
+    public void printStatement(String customerName) {
+        Customer customer = getCustomer(customerName);
+
+        if (customer == null) {
+            return;
+        }
+        System.out.println("_".repeat(30));
+        System.out.println("Customer Name : " + customer.name());
+        System.out.println("Transactions: ");
+
+        for (double transaction : customer.transaction()) { //  unboxing
+            System.out.printf("$%10.2f (%s)%n", transaction, transaction < 0 ? "debit" : "credit");
         }
     }
 }
